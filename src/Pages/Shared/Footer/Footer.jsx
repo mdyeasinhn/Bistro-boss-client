4a452828ed2e75b9c2a7faf1c22ce6f6e06f7665
Md Yeasin <mdyeasinhn@gmail.com>
2024-07-15 00:31:37 +0600

    navbar banner and footer setup complete

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -59,6 +59,11 @@ const Footer = () => {
                     </div>
                 </nav>
             </footer>
+            <div className="footer footer-center bg-base-300 text-base-content p-4">
+                <aside>
+                    <p>Copyright © ${new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+                </aside>
+            </div>
         </div>
     );
 };
