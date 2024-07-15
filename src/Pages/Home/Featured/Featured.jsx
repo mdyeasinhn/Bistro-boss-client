c8b7614760b7a65fdd17b64906cb7825fd783f6e
Md Yeasin <mdyeasinhn@gmail.com>
2024-07-15 16:15:01 +0600

    popularItems and featured items section completed

---
+++ src/Pages/Home/Featured/Featured.jsx
@@ -0,0 +1,23 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import featuredImg from '../../../assets/home/featured.jpg'
+import './featured.css'
+const Featured = () => {
+    return (
+        <div className="featured-item bg-fixed text-white pt-8 my-20">
+            <SectionTitle subHeading='Check it Out' heading='Featured Item'/>
+            <div className="md:flex justify-center items-center bg-slate-500 bg-opacity-40 pb-20 pt-12 px-36">
+                <div>
+                    <img src={featuredImg} alt="" />
+                </div>
+                <div className="md:ml-10 ">
+                    <p>Aug 20, 2029</p>
+                    <p className="uppercase"> where can i get some?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore eaque magnam labore id possimus vel, ad mollitia, hic pariatur officia, provident minus dolorem eveniet. Debitis excepturi corporis temporibus minus placeat.</p>
+                    <button className="btn btn-outline border-0 border-b-4 mt-4 text-white">Read More</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Featured;
--- src/Pages/Shared/MenuItem/MenuItem.jsx
+++ src/Pages/Shared/MenuItem/MenuItem.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
 
-const MenuItem = () => {
+const MenuItem = ({item}) => {
+const {name, image, price, recipe} = item;
     return (
-        <div>
-            
+        <div className="flex space-x-2">
+            <img style={{borderRadius: '0 200px 200px 200px'}} className="w-[100px]" src={image} alt="" />
+            <div>
+                <h3>{name}----------</h3>
+                <p>{recipe}</p>
+            </div>
+            <p className="text-yellow-500">${price}</p>
         </div>
     );
 };
